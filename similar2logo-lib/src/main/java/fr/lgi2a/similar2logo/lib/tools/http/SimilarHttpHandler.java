9f1f2e1e4823367c045231a42bf3d9324b4364b4
Gildas Morvan <gildas.morvan@univ-artois.fr>
2016-03-31 08:31:44 +0200

    Add pause/resume button to web interface and an interactive probe

--- similar2logo-lib/src/main/java/fr/lgi2a/similar2logo/lib/tools/http/SimilarHttpHandler.java
+++ similar2logo-lib/src/main/java/fr/lgi2a/similar2logo/lib/tools/http/SimilarHttpHandler.java
@@ -1,3 +1,49 @@
+/**
+ * Copyright or © or Copr. LGI2A
+ * 
+ * LGI2A - Laboratoire de Genie Informatique et d'Automatique de l'Artois - EA 3926 
+ * Faculte des Sciences Appliquees
+ * Technoparc Futura
+ * 62400 - BETHUNE Cedex
+ * http://www.lgi2a.univ-artois.fr/
+ * 
+ * Email: gildas.morvan@univ-artois.fr
+ * 
+ * Contributors:
+ * 	Gildas MORVAN (creator of the IRM4MLS formalism)
+ * 	Yoann KUBERA (designer, architect and developer of SIMILAR)
+ * 
+ * This software is a computer program whose purpose is to support the 
+ * implementation of Logo-like simulations using the SIMILAR API.
+ * This software defines an API to implement such simulations, and also 
+ * provides usage examples.
+ * 
+ * This software is governed by the CeCILL-B license under French law and
+ * abiding by the rules of distribution of free software.  You can  use, 
+ * modify and/ or redistribute the software under the terms of the CeCILL-B
+ * license as circulated by CEA, CNRS and INRIA at the following URL
+ * "http://www.cecill.info". 
+ * 
+ * As a counterpart to the access to the source code and  rights to copy,
+ * modify and redistribute granted by the license, users are provided only
+ * with a limited warranty  and the software's author,  the holder of the
+ * economic rights,  and the successive licensors  have only  limited
+ * liability. 
+ * 
+ * In this respect, the user's attention is drawn to the risks associated
+ * with loading,  using,  modifying and/or developing or reproducing the
+ * software by the user in light of its specific status of free software,
+ * that may mean  that it is complicated to manipulate,  and  that  also
+ * therefore means  that it is reserved for developers  and  experienced
+ * professionals having in-depth computer knowledge. Users are therefore
+ * encouraged to load and test the software's suitability as regards their
+ * requirements in conditions enabling the security of their systems and/or 
+ * data to be ensured and,  more generally, to use and operate it in the 
+ * same conditions as regards security. 
+ * 
+ * The fact that you are presently reading this means that you have had
+ * knowledge of the CeCILL-B license and that you accept its terms.
+ */
 package fr.lgi2a.similar2logo.lib.tools.http;
 
 import java.io.IOException;
@@ -11,6 +57,7 @@ import com.sun.net.httpserver.HttpHandler;
 
 import fr.lgi2a.similar.microkernel.ISimulationEngine;
 import fr.lgi2a.similar2logo.kernel.initializations.LogoSimulationModel;
+import fr.lgi2a.similar2logo.lib.probes.InteractiveSimulationProbe;
 
 /**
  * The Http handler of the web server.
@@ -23,8 +70,7 @@ import fr.lgi2a.similar2logo.kernel.initializations.LogoSimulationModel;
 @SuppressWarnings("restriction")
 public class SimilarHttpHandler implements HttpHandler {
 
-	private String htmlHeader = "<!DOCTYPE html>\n<html lang='en'> <head> <meta charset='utf-8'> <meta http-equiv='X-UA-Compatible' content='IE=edge'> <meta name='viewport' content='width=device-width, initial-scale=1'> <title>Similar control</title> </head> <body onload='initInterface();'>     <div class='container'>                  <div class='page-header'>              <p class='pull-right text-info' id='simulationState'>Simulation stopped</p>             <h1>Similar control</h1>                     </div>         <div>         </div>         <div>             <button id='startSimulation' type='button' class='btn btn-primary' onclick='startSimulation()'>Start Simulation</button>             <button id='stopSimulation' type='button' class='btn btn-primary' onclick='stopSimulation()'>Stop Simulation</button>         </div>     ";
-	
+	private String htmlHeader = "<!DOCTYPE html>\n<html lang='en'> <head> <meta charset='utf-8'> <meta http-equiv='X-UA-Compatible' content='IE=edge'> <meta name='viewport' content='width=device-width, initial-scale=1'> <title>Similar control</title> </head> <body onload='initInterface();'>     <div class='container'>                  <div class='page-header'>              <p class='pull-right text-info' id='simulationState'>Simulation stopped</p>             <h1>Similar control</h1>                     </div>         <div>         </div>         <div>             <button id='startSimulation' type='button' class='btn btn-primary' onclick='startSimulation()'>Start Simulation</button>             <button id='stopSimulation' type='button' class='btn btn-primary' onclick='stopSimulation()'>Stop Simulation</button>             <button id='pauseSimulation' type='button' class='btn btn-primary' onclick='pauseSimulation()'>Pause/Resume Simulation</button>         </div>     </div>         <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css' integrity='sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7' crossorigin='anonymous'>     <script src='http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js'></script>     <script type='text/javascript'>     function initInterface() {     $('#stopSimulation').prop('disabled', true);     $('#pauseSimulation').prop('disabled', true);     }     </script>     <script type='text/javascript'>         function startSimulation() {             $.get( 'start' );             $('#simulationState').text('Simulation started');             $('#startSimulation').prop('disabled', true);             $('#stopSimulation').prop('disabled', false);             $('#pauseSimulation').prop('disabled', false);         }     </script>     <script type='text/javascript'>     function stopSimulation() {     $.get( 'stop' );     $('#simulationState').text('Simulation stopped');     $('#startSimulation').prop('disabled', false);     $('#stopSimulation').prop('disabled', true);     $('#pauseSimulation').prop('disabled', true);     }     </script>     <script type='text/javascript'>     function pauseSimulation() {     $.get( 'pause' );     $('#startSimulation').prop('disabled', true);     $('#stopSimulation').prop('disabled', false);     $('#pauseSimulation').prop('disabled', false);    }     </script>";
 	private String htmlUserCode;
 	
 	private String htmlFooter = "</div>         <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css' integrity='sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7' crossorigin='anonymous'>     <script src='http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js'></script>     <script type='text/javascript'>     function initInterface() {     $('#stopSimulation').prop('disabled', true);     }     </script>     <script type='text/javascript'>         function startSimulation() {             $.get( 'start' );             $('#simulationState').text('Simulation started');             $('#startSimulation').prop('disabled', true);             $('#stopSimulation').prop('disabled', false);         }     </script>     <script type='text/javascript'>     function stopSimulation() {     $.get( 'stop' );     $('#simulationState').text('Simulation stopped');     $('#startSimulation').prop('disabled', false);     $('#stopSimulation').prop('disabled', true);     }     </script> </body> </html>";
@@ -41,7 +87,14 @@ public class SimilarHttpHandler implements HttpHandler {
 	 */
 	private SimulationExecutionThread simulationThread;
 	
+	/**
+	 * The interactive simulation probe.
+	 */
+	private InteractiveSimulationProbe interactiveSimulationProbe;
+	
 	public SimilarHttpHandler(ISimulationEngine engine, LogoSimulationModel model) {
+		this.interactiveSimulationProbe = new InteractiveSimulationProbe();
+		engine.addProbe("InteractiveSimulation", this.interactiveSimulationProbe);
 		this.engine = engine;
 		this.model = model;
 		this.setHtmlUserCode("");
@@ -67,6 +120,9 @@ public class SimilarHttpHandler implements HttpHandler {
 		} else if (fileName.equals("/stop")) {
 			handleSimulationAbortionRequest( );
 			response = new String("stop").getBytes();
+		} else if (fileName.equals("/pause")) {
+			handleSimulationPauseRequest( );
+			response = new String("pause").getBytes();
 		} else {
 			h.add("Content-Type", "text");
 			if (Paths.get("results"+fileName).toFile().exists()) {
@@ -82,6 +138,12 @@ public class SimilarHttpHandler implements HttpHandler {
 		os.close();
 	}
 	
+	private void handleSimulationPauseRequest() {
+		this.interactiveSimulationProbe.setPaused(
+			!this.interactiveSimulationProbe.isPaused()
+		);	
+	}
+
 	/**
 	 * Manages the creation of a new simulation.
 	 */
