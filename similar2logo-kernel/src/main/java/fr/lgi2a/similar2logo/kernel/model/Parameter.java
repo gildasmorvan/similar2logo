95ee915b56e4f9359e8e21715b2b13973b762c5a
Gildas Morvan <gildas.morvan@univ-artois.fr>
2016-05-16 17:36:14 +0200

    fix javadoc

--- similar2logo-kernel/src/main/java/fr/lgi2a/similar2logo/kernel/model/Parameter.java
+++ similar2logo-kernel/src/main/java/fr/lgi2a/similar2logo/kernel/model/Parameter.java
@@ -53,6 +53,7 @@ import java.lang.annotation.Target;
 
 /**
  * @author <a href="http://www.lgi2a.univ-artois.net/~morvan" target="_blank">Gildas Morvan</a>
+ * 
  * Contains the meta-data associated to a simulation parameter.
  *
  */
