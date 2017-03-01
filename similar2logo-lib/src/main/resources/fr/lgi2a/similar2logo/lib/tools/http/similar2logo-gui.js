28d616775115b0c147d4906e8c8fe560bc64bb92
Gildas Morvan <gildas.morvan@univ-artois.fr>
2017-03-01 14:23:42 +0100

    Adds JSDoc to similar2logo-gui.js

--- similar2logo-lib/src/main/resources/fr/lgi2a/similar2logo/lib/tools/http/similar2logo-gui.js
+++ similar2logo-lib/src/main/resources/fr/lgi2a/similar2logo/lib/tools/http/similar2logo-gui.js
@@ -1,11 +1,77 @@
-//Establish the WebSocket connection and set up event handlers
+/**
+ * Copyright or © or Copr. LGI2A
+ *
+ * LGI2A - Laboratoire de Genie Informatique et d'Automatique de l'Artois - EA 3926
+ * Faculte des Sciences Appliquees
+ * Technoparc Futura
+ * 62400 - BETHUNE Cedex
+ * http://www.lgi2a.univ-artois.fr/
+ *
+ * Email: gildas.morvan@univ-artois.fr
+ *
+ * Contributors:
+ * 	Gildas MORVAN (creator of the IRM4MLS formalism)
+ * 	Yoann KUBERA (designer, architect and developer of SIMILAR)
+ *
+ * This software is a computer program whose purpose is to support the
+ * implementation of Logo-like simulations using the SIMILAR API.
+ * This software defines an API to implement such simulations, and also
+ * provides usage examples.
+ *
+ * This software is governed by the CeCILL-B license under French law and
+ * abiding by the rules of distribution of free software.  You can  use,
+ * modify and/ or redistribute the software under the terms of the CeCILL-B
+ * license as circulated by CEA, CNRS and INRIA at the following URL
+ * "http://www.cecill.info".
+ *
+ * As a counterpart to the access to the source code and  rights to copy,
+ * modify and redistribute granted by the license, users are provided only
+ * with a limited warranty  and the software's author,  the holder of the
+ * economic rights,  and the successive licensors  have only  limited
+ * liability.
+ *
+ * In this respect, the user's attention is drawn to the risks associated
+ * with loading,  using,  modifying and/or developing or reproducing the
+ * software by the user in light of its specific status of free software,
+ * that may mean  that it is complicated to manipulate,  and  that  also
+ * therefore means  that it is reserved for developers  and  experienced
+ * professionals having in-depth computer knowledge. Users are therefore
+ * encouraged to load and test the software's suitability as regards their
+ * requirements in conditions enabling the security of their systems and/or
+ * data to be ensured and,  more generally, to use and operate it in the
+ * same conditions as regards security.
+ *
+ * The fact that you are presently reading this means that you have had
+ * knowledge of the CeCILL-B license and that you accept its terms.
+ */
+
+
+/**
+ * The javascript library of the Similar2Logo GUI
+ * 
+ * @author <a href="http://www.lgi2a.univ-artois.net/~morvan" target="_blank">Gildas Morvan</a>
+ * @author <a href="http://www.yoannkubera.net" target="_blank">Yoann Kubera</a>
+ * @author <a href="mailto:Antoine-Lecoutre@outlook.com>Antoine Lecoutre</a>
+ *
+ */
+
+
+/**
+ * Establishes the WebSocket connection and set up event handlers 
+ */
 var webSocket = new WebSocket("ws://" + location.hostname + ":" + location.port + "/webSocket");
 
+/**
+ * Inits the control of the GUI.
+ */
 function initInterface() {
     $('#stopSimulation').prop('disabled', true);
     $('#pauseSimulation').prop('disabled', true);
 }
 
+/**
+ * Starts the simulation.
+ */
 function startSimulation() {
     $.get('start');
     $('#startSimulation').prop('disabled', true);
@@ -13,6 +79,9 @@ function startSimulation() {
     $('#pauseSimulation').prop('disabled', false);
 }
 
+/**
+ * Stops the simulation.
+ */
 function stopSimulation() {
     $.get('stop');
     $('#startSimulation').prop('disabled', false);
@@ -20,6 +89,9 @@ function stopSimulation() {
     $('#pauseSimulation').prop('disabled', true);
 }
 
+/**
+ * Pauses the simulation.
+ */
 function pauseSimulation() {
     $.get('pause');
     $('#startSimulation').prop('disabled', true);
@@ -27,28 +99,49 @@ function pauseSimulation() {
     $('#pauseSimulation').prop('disabled', false);
 }
 
+/**
+ * Exits the simulation.
+ */
 function exitSimulation() {
     stopSimulation();
     $.get('shutdown');
     window.close();
 }
 
+/**
+ * Changes the value of a numeric (i.e., integer or real) parameter.
+ * 
+ * @param parameter the numeric parameter to be updated
+ */
 function updateNumericParameter(parameter) {
     var output = 'setParameter?' + parameter + '=' + $('#' + parameter).val();
     $.get(output);
 }
 
+/**
+ * Changes the value of a boolean parameter.
+ * 
+ * @param parameter the boolean parameter to be updated
+ */
 function updateBooleanParameter(parameter) {
     var output = 'setParameter?' + parameter + '=' + $('#' + parameter).prop('checked');
     $.get(output);
 }
 
+/**
+ * Displays the grid canvas in full screen.
+ */
 function fullScreen() {
     document.getElementById('grid_canvas').webkitRequestFullScreen();
     document.getElementById('grid_canvas').height = screen.availHeight;
     document.getElementById('grid_canvas').width = screen.availHeight;
 }
 
+/**
+ * Displays the simulation data in a canvas identified as <code>grid_canvas</code>
+ * 
+ * @param data the simulation data.
+ */
 function drawCanvas(data) {
     var json = JSON.parse(data),
         canvas = document.getElementById('grid_canvas'),
