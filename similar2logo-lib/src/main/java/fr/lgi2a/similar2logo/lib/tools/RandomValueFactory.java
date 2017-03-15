e0448ee8047f92e82499842fcddba3aade4c53cc
Gildas Morvan <gildas.morvan@univ-artois.fr>
2017-03-15 11:39:19 +0100

    Formats code

--- similar2logo-lib/src/main/java/fr/lgi2a/similar2logo/lib/tools/RandomValueFactory.java
+++ similar2logo-lib/src/main/java/fr/lgi2a/similar2logo/lib/tools/RandomValueFactory.java
@@ -59,6 +59,7 @@ import fr.lgi2a.similar2logo.lib.tools.randomstrategies.SecureRandomBasedRandomV
  * @author <a href="http://www.lgi2a.univ-artois.fr/~morvan/" target="_blank">Gildas Morvan</a>
  */
 public class RandomValueFactory {
+	
 	/**
 	 * The random values generation strategy currently used in the simulation.
 	 * The default strategy is based on a java.security.SecureRandom instance using a seed of 20 bytes.
