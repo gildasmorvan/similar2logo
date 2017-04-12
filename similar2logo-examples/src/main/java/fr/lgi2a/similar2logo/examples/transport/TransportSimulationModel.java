04c54a607419e7dc58f4749f48f1113ffd4acd51
Romain Windels <romainwindels@yahoo.fr>
2017-04-12 11:30:24 +0200

    Adds the links between the points from OSM, but not extern points.

--- similar2logo-examples/src/main/java/fr/lgi2a/similar2logo/examples/transport/TransportSimulationModel.java
+++ similar2logo-examples/src/main/java/fr/lgi2a/similar2logo/examples/transport/TransportSimulationModel.java
@@ -48,6 +48,7 @@ package fr.lgi2a.similar2logo.examples.transport;
 
 import java.awt.geom.Point2D;
 import java.io.File;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
@@ -86,10 +87,35 @@ public class TransportSimulationModel extends LogoSimulationModel {
 	 * The path where are the data to import in the simulation.
 	 */
 	private String data;
+	
+	/**
+	 * Border west of the simulation.
+	 */
+	private int minlon;
+	
+	/**
+	 * Border east of the simulation.
+	 */
+	private int maxlon;
+	
+	/**
+	 * Border north of the simulation
+	 */
+	private int minlat;
+	
+	/**
+	 * Border south of the simulation.
+	 */
+	private int maxlat;
 
 	public TransportSimulationModel(LogoSimulationParameters parameters, String path) {
 		super(parameters);
 		this.data = path;
+		TransportSimulationParameters tsp = (TransportSimulationParameters) parameters;
+		this.minlon =0;
+		this.maxlon = tsp.gridWidth;
+		this.minlat = tsp.gridHeight;
+		this.maxlat = 0;
 	}
 
 	/**
@@ -148,10 +174,6 @@ public class TransportSimulationModel extends LogoSimulationModel {
 			Document doc = db.parse(new File(this.data));
 			Element e = doc.getDocumentElement();
 			NodeList nl = e.getChildNodes();
-			int minlon = 0;
-			int maxlon = tsp.gridWidth;
-			int minlat = tsp.gridHeight;
-			int maxlat = 0;
 			for (int i=0; i < nl.getLength(); i++) {
 				Node n = nl.item(i);
 				if (n.getNodeName().equals("bounds")) {
@@ -166,6 +188,7 @@ public class TransportSimulationModel extends LogoSimulationModel {
 			}
 			//We search the nodes that belong to the street
 			Set<String> streets = new HashSet<>();
+			List<List<String>> ptsToLink = new ArrayList<>();
 			for (int i=0; i < nl.getLength(); i++) {
 				Node n = nl.item(i);
 				//We recover the way of the streets
@@ -173,11 +196,14 @@ public class TransportSimulationModel extends LogoSimulationModel {
 					NodeList way = n.getChildNodes();
 					for (int j=0; j < way.getLength(); j++) {
 						if (way.item(j).getNodeName().equals("tag") && way.item(j).getAttributes().getNamedItem("k").getNodeValue().equals("highway")) {
+							List<String> pts = new ArrayList<>();
 							for (int k=0; k < way.getLength(); k++) {
 								if (way.item(k).getNodeName().equals("nd")) {
 									streets.add(way.item(k).getAttributes().getNamedItem("ref").getNodeValue());
+									pts.add(way.item(k).getAttributes().getNamedItem("ref").getNodeValue());
 								}
 							}
+							ptsToLink.add(pts);
 						}
 					}
 				}
@@ -199,11 +225,90 @@ public class TransportSimulationModel extends LogoSimulationModel {
 					}
 				}
 			}
+			linkPoints (ptsToLink, environment);
 			return eid;
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return null;
 	}
+	
+	/**
+	 * Determines the point to link
+	 * @param pts the list of vectors of points
+	 * @param lep the environment
+	 */
+	protected void linkPoints (List<List<String>> pts, LogoEnvPLS lep) {
+		for (List<String> liste : pts) {
+			for (int i=0; i < liste.size() -1; i++) {
+				Point2D ori = getCoordinates(data, liste.get(i));
+				Point2D des = getCoordinates(data, liste.get(i+1));
+				if ((ori != null) && (des != null))
+					printRoadBetweenTwoPoints(ori, des, lep);
+			}
+		}
+	}
+	
+	/**
+	 * Gives the Point2D associates to a id in the file
+	 * @param file data from OSM
+	 * @param id the id of the point to search
+	 * @return the coordinates of the id
+	 */
+	protected Point2D getCoordinates (String file, String id) {
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+		try {
+			DocumentBuilder db = dbf.newDocumentBuilder();
+			Document doc = db.parse(new File(this.data));
+			Element e = doc.getDocumentElement();
+			NodeList nl = e.getChildNodes();
+			for (int i=0; i < nl.getLength(); i++) {
+				Node n = nl.item(i);
+				if (n.getNodeName().equals("node") && n.getAttributes().getNamedItem("id").getNodeValue().equals(id)) {
+					int lat = (int) (Double.parseDouble(n.getAttributes().getNamedItem("lat").getNodeValue())*Math.pow(10, 7));
+					int lon = (int) (Double.parseDouble(n.getAttributes().getNamedItem("lon").getNodeValue())*Math.pow(10, 7));
+					if ((lat >= minlat) && (lat <= maxlat) && (lon >= minlon) && (lon <= maxlon)) {
+						int x = Math.round((lon - minlon)/100);
+						int y = Math.round((maxlat - lat)/100);
+						Point2D pt = new Point2D.Double((double) x, (double) y);
+						return pt;
+					}
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	/**
+	 * Add the street marks between ori and des
+	 * @param ori the point of origin
+	 * @param des the point of destination
+	 * @param lep the environment
+	 */
+	protected void printRoadBetweenTwoPoints (Point2D ori, Point2D des, LogoEnvPLS lep) {
+		if (!ori.equals(des)) {
+			//we test all the 8 directions for knowing what is the best way
+			Point2D nextPosition = new Point2D.Double(ori.getX()+1, ori.getY()); //We start by the south
+			double bestDistance = Point2D.distance(nextPosition.getX(), nextPosition.getY(), des.getX(), des.getY());
+			for (int i = -1 ; i <=1; i++) {
+				for (int j= -1; j <= 1; j++) {
+					if (!((i ==0) && (j ==0)) && !((i == 1) && (j == 0))) {
+						Point2D testPoint = new Point2D.Double(ori.getX()+i, ori.getY()+j);
+						double distance = Point2D.distance(testPoint.getX(), testPoint.getY(), des.getX(), des.getY());
+						if (distance < bestDistance) {
+							nextPosition = testPoint;
+							bestDistance = distance;
+						}
+					}
+				}
+			}
+			lep.getMarksAt((int) nextPosition.getX(), (int) nextPosition.getY() ).add(new Mark<Double>(nextPosition, (double) 0, "Street"));
+			printRoadBetweenTwoPoints(nextPosition, des, lep);
+		}
+	}
+	
+
 
 }
