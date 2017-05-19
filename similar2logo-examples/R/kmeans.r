ee9e8624f3d49324ca1835de245b6967c505885d
Gildas Morvan <gildas.morvan@univ-artois.fr>
2017-05-19 10:50:41 +0200

    Corrects a bug in kmeans.r that caused renjin to crash when a cluster contains only 1 value

--- similar2logo-examples/R/kmeans.r
+++ similar2logo-examples/R/kmeans.r
@@ -3,9 +3,7 @@
 #Catégorisation de la base
 
 k=10 #nombre de clusters
-
 datappCluster <- kmeans(datapp, k, nstart = 20) #catégorisation de la base
-
 #récupération des données originaux pour les centres des 10 clusters, ony applied if datapp
 #is scaled
 #datappCluster$centers = t(apply(datappCluster$centers,1,function(r)r*
@@ -20,7 +18,12 @@ for (column in colnames(datapp)){
 observations_to_keep=vector()# initialisation du vecteur des observations à conserver
 for(i in 1:k){ #l'operation se fait pour les 10 clusters
   cluster=datapp[which(datappCluster$cluster %in% i),] #cluster i
-  if (duplicated(rbind(cluster, datappCluster$centers[i,]))[nrow(cluster)+1]){ 
+  if(NCOL(cluster)==1) {
+  	observations_to_keep=c(observations_to_keep, #si le centre i du cluster i est une observation
+                           which(apply(datapp,   #alors on conserve son indice
+                                       1, 
+                                       function(a)all(a==datappCluster$centers[i,])) == T)[[1]])
+  } else if (duplicated(rbind(cluster, datappCluster$centers[i,]))[nrow(cluster)+1]){ 
     observations_to_keep=c(observations_to_keep, #si le centre i du cluster i est une observation
                            which(apply(datapp,   #alors on conserve son indice
                                        1, 
@@ -35,6 +38,5 @@ for(i in 1:k){ #l'operation se fait pour les 10 clusters
                                        function(a)all(a==cluster[which(distances == min(distances))[[1]],])) == T)[[1]])
   }
 }
-
 # le vecteur observations.to.keep avec 10 indices d'observations à conserver (pour pas de temps 1000) 
 observations_to_keep
