ca0204772ec383884a36206f2b4f728fb61db51b
Romain Windels <romainwindels@yahoo.fr>
2017-10-26 11:09:06 +0200

    Solves the problem with the test.

--- similar2logo-examples/src/test/java/fr/lgi2a/similar2logo/examples/transport/osm/roadsgraph/RoadGraphTest.java
+++ similar2logo-examples/src/test/java/fr/lgi2a/similar2logo/examples/transport/osm/roadsgraph/RoadGraphTest.java
@@ -19,19 +19,19 @@ public class RoadGraphTest extends TestCase {
 		RoadNode rn6 = new RoadNode (new Point2D.Double(2,2));
 		RoadNode rn7 = new RoadNode (new Point2D.Double(0, 5));
 		RoadNode rn8 = new RoadNode (new Point2D.Double(1, 6));
-		rg.addRoadEdge(new RoadEdge(rn1, rn2,"Street"));
-		rg.addRoadEdge(new RoadEdge(rn2,rn3,"Street"));
-		rg.addRoadEdge(new RoadEdge(rn3, rn4,"Street"));
-		rg.addRoadEdge(new RoadEdge (rn3,rn5,"Street"));
-		rg.addRoadEdge(new RoadEdge(rn3,rn6,"Street"));
-		rg.addRoadEdge(new RoadEdge(rn5, rn6,"Street"));
-		rg.addRoadEdge(new RoadEdge (rn1, rn7,"Street"));
+		rg.addRoadEdge(new RoadEdge(rn1, rn2,"Secondary"));
+		rg.addRoadEdge(new RoadEdge(rn2,rn3,"Secondary"));
+		rg.addRoadEdge(new RoadEdge(rn3, rn4,"Secondary"));
+		rg.addRoadEdge(new RoadEdge (rn3,rn5,"Secondary"));
+		rg.addRoadEdge(new RoadEdge(rn3,rn6,"Secondary"));
+		rg.addRoadEdge(new RoadEdge(rn5, rn6,"Secondary"));
+		rg.addRoadEdge(new RoadEdge (rn1, rn7,"Secondary"));
 		rg.addRoadEdge(new RoadEdge(rn4, rn7, "Tramway"));
-		rg.addRoadEdge(new RoadEdge(rn7, rn8, "Street"));
+		rg.addRoadEdge(new RoadEdge(rn7, rn8, "Secondary"));
 	}
 	
 	public void testWayToGo () {
-		List<Point2D> res = rg.wayToGo(new Point2D.Double(0.9, 6), new Point2D.Double(0.1, 0));
+		List<Point2D> res = rg.wayToGo(new Point2D.Double(0.1, 0), new Point2D.Double(0.9, 6));
 		for (Point2D p : res) {
 			System.out.println(p.toString());
 		}
