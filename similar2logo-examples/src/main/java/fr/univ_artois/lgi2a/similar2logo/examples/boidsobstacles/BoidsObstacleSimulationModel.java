511bcce5ad53de6417c5a9898bb2f72d430da15e
Gildas Morvan <gildas.morvan@univ-artois.fr>
2019-03-25 18:05:35 +0100

    Fix code smells

--- similar2logo-examples/src/main/java/fr/univ_artois/lgi2a/similar2logo/examples/boidsobstacles/BoidsObstacleSimulationModel.java
+++ similar2logo-examples/src/main/java/fr/univ_artois/lgi2a/similar2logo/examples/boidsobstacles/BoidsObstacleSimulationModel.java
@@ -1,4 +1,50 @@
-package fr.univ_artois.lgi2a.similar2logo.examples.boidObstacle;
+/**
+ * Copyright or © or Copr. LGI2A
+ * 
+ * LGI2A - Laboratoire de Genie Informatique et d'Automatique de l'Artois - EA 3926 
+ * Faculte des Sciences Appliquees
+ * Technoparc Futura
+ * 62400 - BETHUNE Cedex
+ * http://www.lgi2a.univ-artois.fr/
+ * 
+ * Email: gildas.morvan@univ-artois.fr
+ * 
+ * Contributors:
+ * 	Gildas MORVAN (creator of the IRM4MLS formalism)
+ * 	Yoann KUBERA (designer, architect and developer of SIMILAR)
+ * 
+ * This software is a computer program whose purpose is to support the
+ * implementation of multi-agent-based simulations using the formerly named
+ * IRM4MLS meta-model. This software defines an API to implement such 
+ * simulations, and also provides usage examples.
+ * 
+ * This software is governed by the CeCILL-B license under French law and
+ * abiding by the rules of distribution of free software.  You can  use, 
+ * modify and/ or redistribute the software under the terms of the CeCILL-B
+ * license as circulated by CEA, CNRS and INRIA at the following URL
+ * "http://www.cecill.info". 
+ * 
+ * As a counterpart to the access to the source code and  rights to copy,
+ * modify and redistribute granted by the license, users are provided only
+ * with a limited warranty  and the software's author,  the holder of the
+ * economic rights,  and the successive licensors  have only  limited
+ * liability. 
+ * 
+ * In this respect, the user's attention is drawn to the risks associated
+ * with loading,  using,  modifying and/or developing or reproducing the
+ * software by the user in light of its specific status of free software,
+ * that may mean  that it is complicated to manipulate,  and  that  also
+ * therefore means  that it is reserved for developers  and  experienced
+ * professionals having in-depth computer knowledge. Users are therefore
+ * encouraged to load and test the software's suitability as regards their
+ * requirements in conditions enabling the security of their systems and/or 
+ * data to be ensured and,  more generally, to use and operate it in the 
+ * same conditions as regards security. 
+ * 
+ * The fact that you are presently reading this means that you have had
+ * knowledge of the CeCILL-B license and that you accept its terms.
+ */
+package fr.univ_artois.lgi2a.similar2logo.examples.boidsobstacles;
 
 import java.awt.geom.Point2D;
 import java.util.Map;
@@ -9,9 +55,9 @@ import fr.univ_artois.lgi2a.similar.microkernel.AgentCategory;
 import fr.univ_artois.lgi2a.similar.microkernel.LevelIdentifier;
 import fr.univ_artois.lgi2a.similar.microkernel.agents.IAgent4Engine;
 import fr.univ_artois.lgi2a.similar.microkernel.levels.ILevel;
-import fr.univ_artois.lgi2a.similar2logo.examples.boidObstacle.model.BoidDecisionObstacleModel;
-import fr.univ_artois.lgi2a.similar2logo.examples.boidObstacle.model.BoidSimulationObstacleParameters;
 import fr.univ_artois.lgi2a.similar2logo.examples.boids.BoidsSimulationModel;
+import fr.univ_artois.lgi2a.similar2logo.examples.boidsobstacles.model.BoidDecisionObstacleModel;
+import fr.univ_artois.lgi2a.similar2logo.examples.boidsobstacles.model.BoidSimulationObstacleParameters;
 import fr.univ_artois.lgi2a.similar2logo.kernel.model.LogoSimulationParameters;
 import fr.univ_artois.lgi2a.similar2logo.kernel.model.agents.turtle.TurtleAgentCategory;
 import fr.univ_artois.lgi2a.similar2logo.kernel.model.agents.turtle.TurtleFactory;
@@ -26,9 +72,10 @@ public class BoidsObstacleSimulationModel extends BoidsSimulationModel {
 		super(parameters);
 	}
 
-	/* (non-Javadoc)
-	 * @see fr.univ_artois.lgi2a.similar2logo.kernel.initializations.AbstractLogoSimulationModel#generateEnvironment(fr.univ_artois.lgi2a.similar.extendedkernel.simulationmodel.ISimulationParameters, java.util.Map)
+	/**
+	 * {@inheritDoc}
 	 */
+	@Override
 	protected EnvironmentInitializationData generateEnvironment(
 			ISimulationParameters simulationParameters,
 			Map<LevelIdentifier, ILevel> levels) {
@@ -43,31 +90,29 @@ public class BoidsObstacleSimulationModel extends BoidsSimulationModel {
 		 */
 		
 		double x;
-		for(x = 10; x < 100; x += 10)
-		{
+		//Vertical line
+		for(x = 10; x < 100; x += 10) {
 			for (double y = 0; y < 10 ; y+=0.1) {
-
 				environment.getMarksAt(x, y).add(new Mark<>(new Point2D.Double(x, y)));
 				
 			}
 			
-		} // Tracé verticale
+		} 
 		
 		double a = 20;
-			for(int u = 0; u<=3; u++)
-			{
+		//Horizontal line
+		for(int u = 0; u<=3; u++) {
 			int y = 10; 
 			for (x = a; x < (a+5) ; x+=0.1) {
-
 				environment.getMarksAt(x, y).add(new Mark<>(new Point2D.Double(x, y)));
 			}
 			a += 20;
 			
-		} // Tracé horizontale
+		} 
 		
 		a = 5;
-		for(int u = 0; u<=3; u++)
-		{
+		//Horizontal line middle
+		for(int u = 0; u<=3; u++) {
 			int y = 15; 
 			for (x = a; x < (a+10) ; x+=0.1) {
 
@@ -75,63 +120,54 @@ public class BoidsObstacleSimulationModel extends BoidsSimulationModel {
 			}
 			a += 25;
 			
-		} // Tracé horizontale milieu
+		} 	
 		
-		
-		
-		for(x = 5; x < 100; x += 15)
-		{
+		for(x = 5; x < 100; x += 15) {
 			for (double y = 20; y < 30 ; y++) {
-
 				environment.getMarksAt(x, y).add(new Mark<>(new Point2D.Double(x, y)));
 			}
 			
 		}
 		
 		a = 10;
-		for(int u = 0; u<=3; u++)
-		{
+		//Horizontal line
+		for(int u = 0; u<=3; u++) {
 			int y = 20; 
 			for (x = a; x < (a+10) ; x+=0.1) {
-
 				environment.getMarksAt(x, y).add(new Mark<>(new Point2D.Double(x, y)));
 			}
 			a += 20;
 			
-		} // Tracé horizontale
+		} 
 		
-		for(x = 15; x < 100; x += 15)
-		{
+		for(x = 15; x < 100; x += 15) {
 			for (double y = 40; y < 55 ; y++) {
-
 				environment.getMarksAt(x, y).add(new Mark<>(new Point2D.Double(x, y)));
 			}
 			
 		}
 		
 		a = 10;
-		for(int u = 0; u<=2; u++)
-		{
+		//Horizontal line middle
+		for(int u = 0; u<=2; u++) {
 			int y = 35; 
 			for (x = a; x < (a+20) ; x+=0.1) {
-
 				environment.getMarksAt(x, y).add(new Mark<>(new Point2D.Double(x, y)));
 			}
 			a += 30;
 		
-		} // Tracé horizontale milieu
+		} 
 		
-		
-		for(x = 45; x <= 60; x++)
-		{
+		//Horizontal line
+		for(x = 45; x <= 60; x++) {
 			int y = 40;
 			environment.getMarksAt(x, y).add(new Mark<>(new Point2D.Double(x, y)));
-		} // Tracé horizontale
+		} 
 		
 		
 		a = 15;
-		for(int u = 0; u<2; u++)
-		{
+		//Horizontal line
+		for(int u = 0; u<2; u++) {
 			int y = 55; 
 			for (x = a; x < (a+16) ; x+=0.1) {
 
@@ -139,21 +175,18 @@ public class BoidsObstacleSimulationModel extends BoidsSimulationModel {
 			}
 			a += 60;
 			
-		} // Tracé horizontale
+		} 
 		
 		
-		for(x = 7; x < 100; x += 14)
-		{
+		for(x = 7; x < 100; x += 14){
 			for (int y = 60; y < 70 ; y++) {
-
 				environment.getMarksAt(x, y).add(new Mark<>(new Point2D.Double(x, y)));
-			}
-			
+			}	
 		}
 		
 		a = 21;
-		for(int u = 0; u<=2; u++)
-		{
+		//Horizontal line
+		for(int u = 0; u<=2; u++) {
 			int y = 60; 
 			for (x = a; x < (a+7) ; x+=0.1) {
 
@@ -161,11 +194,11 @@ public class BoidsObstacleSimulationModel extends BoidsSimulationModel {
 			}
 			a += 28;
 			
-		} // Tracé horizontale
+		} 
 		
 		a = 7;
-		for(int u = 0; u<=2; u++)
-		{
+		//Horizontal line
+		for(int u = 0; u<=2; u++) {
 			int y = 70; 
 			for (x = a; x < (a+7) ; x+=0.1) {
 
@@ -173,11 +206,11 @@ public class BoidsObstacleSimulationModel extends BoidsSimulationModel {
 			}
 			a += 28;
 			
-		} // Tracé horizontale
+		} 
 	
 		a = 0;
-		for(int u = 0; u<=2; u++)
-		{
+		//Horizontal line middle
+		for(int u = 0; u<=2; u++) {
 			int y = 75; 
 			for (x = a; x < (a+25) ; x+=0.1) {
 
@@ -185,10 +218,9 @@ public class BoidsObstacleSimulationModel extends BoidsSimulationModel {
 			}
 			a += 35;
 			
-		} // Tracé horizontale milieu
+		} 
 		
-		for(x = 10; x < 100; x += 20)
-		{
+		for(x = 10; x < 100; x += 20) {
 			for (int y = 80; y < 100 ; y++) {
 
 				environment.getMarksAt(x, y).add(new Mark<>(new Point2D.Double(x, y)));
@@ -196,17 +228,16 @@ public class BoidsObstacleSimulationModel extends BoidsSimulationModel {
 			
 		}
 		
-		for(x = 30; x <= 50; x++)
-		{
+		//Horizontal line
+		for(x = 30; x <= 50; x++) {
 			int y = 80;
 			environment.getMarksAt(x, y).add(new Mark<>(new Point2D.Double(x, y)));
-		} // Tracé horizontale
+		} 
 		
-		for(x = 70; x <= 90; x++)
-		{
+		for(x = 70; x <= 90; x++) {
 			int y = 80;
 			environment.getMarksAt(x, y).add(new Mark<>(new Point2D.Double(x, y)));
-		} // Tracé horizontale
+		} //Horizontal line
 
 
 		return environmentInitializationData;
@@ -215,8 +246,7 @@ public class BoidsObstacleSimulationModel extends BoidsSimulationModel {
 	/**
 	 * {@inheritDoc}
 	 */
-	@Override
-	
+	@Override	
 	protected AgentInitializationData generateAgents(
 			ISimulationParameters parameters,
 			Map<LevelIdentifier, ILevel> levels) {
